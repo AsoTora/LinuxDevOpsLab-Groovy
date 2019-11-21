079e2b489a8eff1faaa99103eaaa17a92a625383
student <andrei_shvedau@epam.com>
2019-11-21 14:42:11 +0300

    fixed task 8

--- tasks/task8.groovy
+++ tasks/task8.groovy
@@ -1,9 +1,10 @@
 import groovy.json.JsonSlurper 
+import groovy.json.JsonOutput 
 
 def parseAndFilterJson(string){
     def jsonSlurper = new JsonSlurper()
     def object = jsonSlurper.parseText(string)
-    return object.findAll { Integer.toString(it.value).collect { Integer.parseInt(it) }.sum() == 9 }
+    return JsonOutput.toJson(object.findAll { Integer.toString(it.value).collect { Integer.parseInt(it) }.sum() == 9 })
 }
 
 return this
