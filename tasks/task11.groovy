c86da7e81780d1bbdda9c666f7f99cc954658597
student <andrei_shvedau@epam.com>
2019-11-24 20:50:34 +0300

    task 11

--- tasks/task11.groovy
+++ tasks/task11.groovy
@@ -1,5 +1,8 @@
-def gstring(){
-
+def gstring2(inp, num) {
+    def list = []
+    for(int x = 1; x <= 3; x++)
+        x == num ? list.add(x.toString() + "(" + inp + ")") : list.add(x.toString() + "()")  
+    return list.join(" ")
 }
 
 return this
